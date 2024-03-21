75c0ad5c8a83bd33a4939091c438461f14011712
Leopold Seidenglanz <leopold.seidenglanz@elev.ga.ntig.se>
2024-03-21 11:00:10 +0100

    first commit

---
+++ InvesteringsSpel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Raylib_cs;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+Raylib.InitWindow(640, 1000, "Spel");
+
+
+while (!Raylib.WindowShouldClose())
+    {
+        Hus hus = new Hus();
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.Gray);
+        Raylib.DrawRectangle(10, 910, 620, 80, Color.DarkGray);
+        Raylib.DrawRectangle(508, 920, 112, 60, Color.Gray);
+        Raylib.DrawRectangle(386, 920, 112, 60, Color.Gray);
+        Raylib.DrawRectangle(264, 920, 112, 60, Color.Gray);
+        Raylib.DrawRectangle(142, 920, 112, 60, Color.Gray);
+        Raylib.DrawRectangle(20, 920, 112, 60, Color.Gray);
+        Raylib.EndDrawing();
+    }
+    Raylib.CloseWindow();
